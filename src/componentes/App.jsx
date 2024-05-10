156dfac2e68be363ae6a57f20a67db4c35ec8d74
WaraChuquimia <estrellachuquimia0404@gmail.com>
2024-05-10 13:30:21 -0400

    cuartocomponte

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className='card'>
-        <img src="./image/react2.png" alt="" className='imagen' />
+        <img src="./imagen/react2.png" alt="" className='imagen' />
         <div className='Textos'>
           <p className='terapia2'>Terapia 2</p>
           <p className='ejercicios'>Ejercicios pelvicos</p>
