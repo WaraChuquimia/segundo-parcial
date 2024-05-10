3ef1707fd093e757f4d3d76d5d345f8a218a0401
WaraChuquimia <estrellachuquimia0404@gmail.com>
2024-05-10 13:34:34 -0400

    renombramiento

---
+++ src/componentes/card.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='card'>
+        <img src="./imagen/imagen.png" alt="" className='imagen' />
+        <div className='Texto'>
+          <p className='terapia2'>Terapia 2</p>
+          <p className='ejercicios'>Ejercicios pelvicos</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
